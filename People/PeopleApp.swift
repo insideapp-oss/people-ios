fabc6382149676cd3bd1cd22149ba11de3cb3420
David Yang <david.yang@insideapp.fr>
2020-09-28 15:38:32 +0200

    Initial Commit

---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by David Yang on 28/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
